3c08cdeb225027c16b70f9e7ac3b9806e54b2bfe
miguelhuerta <guelme88@gmail.com>
2023-06-05 13:13:24 -0700

    wrapping up Repository and Controller endpoints

--- src/main/java/example/cashcard/controllers/CashCardController.java
+++ src/main/java/example/cashcard/controllers/CashCardController.java
@@ -1,23 +1,39 @@
 package example.cashcard.controllers;
 
 import example.cashcard.models.CashCard;
+import example.cashcard.repositories.CashCardRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    private final CashCardRepository cashCardRepository;
+
     @GetMapping("/{requestId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
-        if (requestId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L,123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = this.cashCardRepository.findById(requestId);
+
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> createCashCard(@RequestBody CashCard newCardCashRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = this.cashCardRepository.save(newCardCashRequest);
+        URI locationOfNewCashCard = ucb.path("cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
--- src/main/java/example/cashcard/models/CashCard.java
+++ src/main/java/example/cashcard/models/CashCard.java
@@ -1,3 +1,5 @@
 package example.cashcard.models;
 
-public record CashCard(Long id, Double amount) {}
+import org.springframework.data.annotation.Id;
+
+public record CashCard(@Id Long id, Double amount) {}
